d51f150cb2f938962c9fb4f6074ebd0ca404371d
Reynaldi Wijaya <reynaldiwijaya2306@gmail.com>
2020-03-26 16:25:23 +0700

    Initial Commit

---
+++ Optimin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Optimin
+//
+//  Created by Reynaldi Wijaya on 26/03/20.
+//  Copyright © 2020 Reynaldi Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
